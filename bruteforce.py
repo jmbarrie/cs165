4c08aa87bef8ad23b8b1ca489227cac0f0cd2ae8
Juan Barrientos <jbarr078@ucr.edu>
2021-01-13 11:26:09 -0800

    updated multiprocessing bruteforce

--- bruteforce.py
+++ bruteforce.py
@@ -19,16 +19,17 @@ solution = '$1$' + given_salt + '$' + expected_hash
 num_processes = 10
 max_password_length = 6
 alphabet = string.ascii_lowercase
-hash_output = ''
 start = None
 
 def generate_passwords():
     print('Generating password list')
     start = timeit.default_timer()
     passwords = []
-    for i in range(0, max_password_length + 1):
-        for x in itertools.product(alphabet, repeat=i):
-            passwords.append("".join(x))
+    # for i in range(0, max_password_length + 1):
+        # for x in itertools.product(alphabet, repeat=i):
+            # passwords.append("".join(x))
+    for x in itertools.product(alphabet, repeat=6):
+        passwords.append("".join(x))
 
     end = timeit.default_timer()
     print('Password list generated')
@@ -42,49 +43,42 @@ def brute_force(pool, password_list):
     def callback(result):
         if result:
             print('password cracked')
-            print('Output hash: ', hash_output)
             end = timeit.default_timer()
             print('password cracked in: ', end - start)
-            pool.terminate()
 
     print('Starting brute force')
     start = timeit.default_timer()
+    result_obj = []
+
     for i in range(0, num_processes):
-        print(i)
         if i == num_processes - 1:
             work_split = password_list[partition * i:]
-        elif i == 0:
-            zero_partition = password_list.index('bebwjr')
-            print('bebwjr index: ', zero_partition)
-            zero_split = (partition * i) + zero_partition
-            work_split = password_list[zero_split: partition * (i + 1)]
         else:
             work_split = password_list[partition * i: partition * (i + 1)]
 
-        pool.apply_async(do_job, (work_split, given_salt), callback=callback)
+        result = pool.apply_async(do_job, (work_split, given_salt, solution), callback=callback)
+        result_obj.append(result)
+    
+    for result in result_obj:
+        if result.get():
+            results = result.get()
 
-def do_job(work, salt):
+    if results:
+        print('Results: ', results)
+    pool.close()
+    pool.join()
+
+def do_job(work, salt, solution):
     for i in work:
-        # print('Attempting password: ', i)
         md5 = MD5_Crypt(i, salt, max_password_length)
         output = md5.get_hash()
-        # print('Full output: ', output)
         if output == solution:
-                
-            cracked_password = i
-            hash_output = output
-            print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-            print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-            print('Password cracked: ', i)
-            print('etc/shadow hash calculated: ', hash_output)
-            print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-            print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-            return True 
+            return i
+    
+    return False
     
 if __name__ == "__main__":
     password_list = generate_passwords()
     my_pool = mp.Pool(processes=num_processes)
+    password_list.reverse()
     brute_force(my_pool, password_list)
-    print(hash_output)
-    my_pool.close()
-    my_pool.join()
